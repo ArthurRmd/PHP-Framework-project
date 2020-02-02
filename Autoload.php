cc17fec8a9b350a9ad596501090998ae8c6dfd57
Arthur <arthur.remond@protonmail.com>
2020-02-02 19:29:17 +0100

    WIP

--- Autoload.php
+++ Autoload.php
@@ -7,7 +7,7 @@ class Autoload
     public static function register()
     {
         spl_autoload_register(function($className) {
-            require_once $className . '.php';
+            require_once (lcfirst(str_replace('\\', DS, $className)))    . '.php';
         });
     }
 
